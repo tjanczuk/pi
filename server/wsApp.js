5cabbe247d31a83775249b1effa22dc0b21dcb17
Tomasz Janczuk <tomasz@janczuk.org>
2020-05-11 07:47:01 +0000

    websocet to robot

--- server/wsApp.js
+++ server/wsApp.js
@@ -1,7 +1,83 @@
+const debug = require('debug')('server:server');
+const Robot = require('./robot');
+
+let robot;
+
+const killRobot = () => {
+    if (robot) {
+        robot.destruct();
+        robot = undefined;
+    }
+};
+
+const movements = {
+    '0000' : ['stop', 'stop', 'Stopped!'],
+    '0001' : ['stop', 'backward', 'Turning right by backing up!'],
+    '0010' : ['stop', 'forward', 'Turning left by forwarding!'],
+    '0011' : ['stop', 'stop', `I am confused, I can't go forward and back at the same time!`],
+    '0100' : ['backward', 'stop', 'Turning left by backing up!'],
+    '0101' : ['backward', 'backward', 'Backing up!'],
+    '0110' : ['backward', 'forward', 'Spin left!'],
+    '0111' : ['stop', 'stop', 'I am confused, too many buttons!'],
+    '1000' : ['forward', 'stop', 'Turning right by forwarding!'],
+    '1001' : ['forward', 'backward', 'Spin right!'],
+    '1010' : ['forward', 'forward', 'Forward!'],
+    '1011' : ['stop', 'stop', 'I am confused, too many buttons!'],
+    '1100' : ['stop', 'stop', 'I am confused, too many buttons!'],
+    '1101' : ['stop', 'stop', 'I am confused, too many buttons!'],
+    '1110' : ['stop', 'stop', 'I am confused, too many buttons!'],
+    '1111' : ['stop', 'stop', 'I am confused, too many buttons!'],
+};
+
 module.exports = ws => {
-    console.log('NEW WS CONNECTION!');
+    debug('New websocket connection');
+
+    const message = message => ws.send(JSON.stringify({ message }));
+
+    killRobot();
+    robot = new Robot();
+
+    message('Welcome!');
+
     ws.on('message', m => {
-        console.log('MESSAGE', m);
+        debug('Message', m);
+
+        try {
+            m = JSON.parse(m);
+        }
+        catch (e) {
+            return message(`Sorry, I did not understand that: ${m}`);
+        }
+
+        if (!robot) return;
+
+        if (m.led) {
+            robot.on();
+            message('Light on!');
+        }
+        else {
+            robot.off();
+            message('Light off!');
+        }
+
+        let movementKey = `${m.lf ? '1' : '0'}${m.lb ? '1' : '0'}${m.rf ? '1' : '0'}${m.rb ? '1' : '0'}`;
+        let movement = movements[movementKey];
+        if (!movement) {
+            return message('I did not get the instruction');
+        }
+        debug('MOVEMENT', movement);
+        robot.left[movement[0]]();
+        robot.right[movement[1]]();
+        message(movement[2]);
     });
-    ws.send(JSON.stringify({ foo: 'bar' }));
+
+    ws.on('error', e => {
+        debug('Error', e);
+        killRobot();
+    });
+
+    ws.on('close', () => {
+        debug('Close');
+        killRobot();
+    });
 };
